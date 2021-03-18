b5a38b463e17cb11457eb3a15a75e43fb0dcc8c0
hasankorogluayseozates <69298065+ayseozates@users.noreply.github.com>
2021-03-18 23:50:28 +0300

    18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { ColorComponent } from './components/color/color.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { RentalComponent } from './components/rental/rental.component';
+import{HttpClientModule}from'@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    ColorComponent,
+    NaviComponent,
+    BrandComponent,
+    RentalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/car.ts
@@ -0,0 +1,10 @@
+export interface Car{
+    Id:number;
+    carName:string;
+    colorId:number;
+    brandId:number;
+    description:string;
+    dailyPrice:number;
+    modelYear:string;
+
+}
